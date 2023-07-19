bb09fd989167e4a51743d62efdd3049fc17efed9
Yazan <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-20 01:55:23 +0300

    create car name input field

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,5 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeName } from "../store";
+
+
 function CarForm() {
-  return <div>CarForm</div>;
+  const dispatch = useDispatch();
+
+  const name = useSelector(({form})=>{
+    return form.name;
+  });
+  const onNameChanged = (event) =>{
+    dispatch(changeName(event.target.value));
+  };
+
+
+  return <div className="car-form-panel">
+    <h4 className="subtitle is-3">Add Car</h4>
+    <form>
+      <div className="field-group">
+        <div className="field">
+          <label className="label">Name</label>
+          <input
+          className="input is-expanded"
+          value={name}
+          onChange={onNameChanged}
+          />
+        </div>
+      </div>
+    </form>
+  </div>;
 }
 
 export default CarForm;
