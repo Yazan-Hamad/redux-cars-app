2feaed0a6f4996a258bc54b5dc8415e720044be3
Yazan <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-20 15:45:12 +0300

    create CarList component

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,5 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
+import { removeCar } from "../store";
+
 function CarList() {
-  return <div>CarList</div>;
+  const dispatch = useDispatch();
+  
+  const cars = useSelector((state)=>{
+    return state.cars.data;
+  });
+
+  const onCarDelete = (car)=>{
+    dispatch(removeCar(car.id));
+  };
+
+  const fetchCars = cars.map((car)=>{
+    return (
+      <div key={car.id} className="panel">
+        <p>{car.name} - ${car.cost}</p>
+        <button 
+        className="button is-danger"
+        onClick={()=>onCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    )
+  })
+
+  return (<div className="car-list">
+    {
+      fetchCars
+    }
+    <hr/>
+  </div>);
 }
 
 export default CarList;
