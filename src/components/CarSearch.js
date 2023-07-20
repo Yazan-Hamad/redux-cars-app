9fd1348779bdc64f0ec441b32b8fcde2ef196984
Yazan <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-20 20:15:30 +0300

    create CarSearch component

--- src/components/CarSearch.js
+++ src/components/CarSearch.js
@@ -1,5 +1,27 @@
+import { useDispatch,useSelector } from "react-redux";
+import { changeSearchTerm } from "../store";
+
 function CarSearch() {
-  return <div>CarSearch</div>;
+  const dispatch = useDispatch();
+  const searchTerm = useSelector((state)=>{
+    return state.cars.searchTerm;
+  });
+  const onSearchTermChange = (event)=>{
+    dispatch(changeSearchTerm(event.target.value));
+  }
+  return (
+  <div className="list-header">
+    <h3 className="title is-3">My cars</h3>
+    <div className="search field is-horizantal">
+      <label className="label">Search</label>
+      <input 
+      className="input"
+      value={searchTerm}
+      onChange={onSearchTermChange}
+      />
+    </div>
+  </div>
+  );
 }
 
 export default CarSearch;
