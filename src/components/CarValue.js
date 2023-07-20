0a09948d07c446ff1b2865b880ee34ace449a1e5
Yazan <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-20 20:15:55 +0300

    Create car value component

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,5 +1,15 @@
+import { useSelector } from "react-redux";
+
 function CarValue() {
-  return <div>CarValue</div>;
+  const totlCost = useSelector(({ cars: { data, searchTerm } }) =>
+    data
+      .filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((cost, car) => cost + car.cost, 0)
+  );
+
+  return <div className="car-value">Total Cost: ${totlCost}</div>;
 }
 
 export default CarValue;
